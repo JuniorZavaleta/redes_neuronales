2e589eeea29731eea86dbfec23793bd2f4592fed
Junior Claudio <juniorclaudiozavaleta@gmail.com>
2018-04-13 23:46:48 -0500

    Fix weights and always show results after training

--- PortsPerceptron.py
+++ PortsPerceptron.py
@@ -12,19 +12,21 @@ def print_line():
 class Perceptron:
     weights = []
 
-    def __init__(self, values_x, values_y, weights, threshold, activation_function):
+    def __init__(self, values_x, values_y, weights, threshold, activation_function, message=None):
         """
         :param values_x: Input values
         :param values_y: Expected values
         :param weights: Weight for each value
         :param threshold:
         :param activation_function: Function to activate :v
+        :param message Title when the perceptron show the results of training
         """
         self.values_x = values_x
         self.values_y = values_y
         self.weights = weights
         self.threshold = threshold
         self.activation_function = activation_function
+        self.message = message if message is not None else "Results"
 
     def training(self):
         has_error = True
@@ -33,6 +35,7 @@ class Perceptron:
             has_error = False
             print("# Iteration " + str(it))
             print("X{0}|d |y | W".format((len(self.weights)*3-1)*" "))
+
             for i in range(len(self.values_x)):
                 dot_product = sum(value * weight for value, weight in zip(self.values_x[i], self.weights))
                 dot_product = dot_product + (self.values_y[i] * self.threshold)
@@ -48,13 +51,15 @@ class Perceptron:
                     has_error = True
 
             it = it + 1
-            # has_error = False
+        self.print_results_training()
+
+    def print_results_training(self):
+        print_line()
+        print("{0}: {1}".format(self.message, self.weights))
+        print_line()
 
 
 # Port with 2 elements
-W = [0, -1]
-b = 0.25
-
 X = [
     [0, 0],
     [0, 1],
@@ -66,21 +71,25 @@ X = [
 print("AND")
 print_line()
 Y = [0, 0, 0, 1]
-perceptron_and = Perceptron(X, Y, W, b, step_function)
+perceptron_and = Perceptron(values_x=X,
+                            values_y=Y,
+                            weights=[0, -1],
+                            threshold=0.25,
+                            activation_function=step_function,
+                            message="Final Weights AND for 2 elements")
 perceptron_and.training()
-print_line()
-print("Final Weights OR for 3 elements: {0}".format(perceptron_and.weights))
-print_line()
 
 # OR
 Y = [0, 1, 1, 1]
 print("OR")
 print_line()
-perceptron_or = Perceptron(X, Y, W, b, step_function)
+perceptron_or = Perceptron(values_x=X,
+                           values_y=Y,
+                           weights=[0, -1],
+                           threshold=0.25,
+                           activation_function=step_function,
+                           message="Final Weights OR for 2 elements")
 perceptron_or.training()
-print_line()
-print("Final Weights OR for 3 elements: {0}".format(perceptron_or.weights))
-print_line()
 
 # Port with 2 elements
 X3 = [
@@ -93,25 +102,25 @@ X3 = [
     [1, 1, 0],
     [1, 1, 1]
 ]
-W3 = [-1, -1, 0]
-b3 = -0.25
 
 # OR 3 elements
 Y3 = [0, 1, 1, 1, 1, 1, 1, 1]
 print("OR 3 elements")
 print_line()
-perceptron_or_3 = Perceptron(X3, Y3, W3, b3, step_function)
+perceptron_or_3 = Perceptron(values_x=X3,
+                             values_y=Y3,
+                             weights=[-1, -1, 0],
+                             threshold=-0.25,
+                             activation_function=step_function)
 perceptron_or_3.training()
-print_line()
-print("Final Weights OR for 3 elements: {0}".format(perceptron_or_3.weights))
-print_line()
 
 # AND 3 elements
 Y3 = [0, 0, 0, 0, 0, 0, 0, 1]
 print("AND 3 elements")
 print_line()
-perceptron_and_3 = Perceptron(X3, Y3, W3, b3, step_function)
+perceptron_and_3 = Perceptron(values_x=X3,
+                              values_y=Y3,
+                              weights=[-1, -1, 0],
+                              threshold=-0.25,
+                              activation_function=step_function)
 perceptron_and_3.training()
-print_line()
-print("Final Weights AND for 3 elements: {0}".format(perceptron_and_3.weights))
-print_line()
